521241faf719255366af26867cd51b78b79234ad
annablanc <blink1986@gmail.com>
2015-05-13 10:48:45 +0200

    Implementación tests
    Implementación tests

--- src/com/example/tests/AltaContactoTest.java
+++ src/com/example/tests/AltaContactoTest.java
@@ -11,6 +11,29 @@ public class AltaContactoTest extends ActivityInstrumentationTestCase2<AltaConta
 	 * 
 	 * Este clase de test se usa para realizar tests en la actividad AltaContacto
 	 * 
+	 * Tests de la función validar comprobarContacto
+	 * 		inicio --> 1 path
+	 * 		existen 2 if's --> + 2 paths 
+	 * 		existe 1 while --> + 1 path
+	 * 		existe 1 if dentro del while --> + 1 path
+	 * 		
+	 * 		total paths ==> 5 
+	 * 
+	 * 		Paths:
+	 * 			(1.1) Test AltaContacto (linea 201) retorna true
+	 *			(1.2) Test AltaContacto (linea 201) retorna false
+	 * 			(2.1) Test AltaContacto (linea 206) retorna true
+	 * 			(2.2) Test AltaContacto (linea 206) retorna false
+	 * 			(3.1) Test AltaContacto (linea 211) retorna true		
+	 * 					(debido a que depende de los datos de la BD siempre retorna false)
+	 * 			(3.2) Test AltaContacto (linea 211) retorna false
+	 * 
+	 * 			(debido a que depende de los datos de la BD no se pueden probar)
+	 * 			(4.1) Test AltaContacto (linea 213) retorna true
+	 * 			(4.2) Test AltaContacto (linea 213) retorna false
+	 * 
+	 * 
+	 * 
 	 * A su vez también se estan realizando tests 
 	 * a las funciones getter/setter de la clase ContactoAgenda
 	 */
@@ -43,31 +66,128 @@ public class AltaContactoTest extends ActivityInstrumentationTestCase2<AltaConta
 	
 	public void testMailVacio() throws Exception
 	{
+		/*
+		 *
+		 *  (1.1) Test AltaContacto (linea 201) retorna true
+		 * 
+		 */
 		
+		boolean result;
+
+		contacto.setMail("");
+		result = alta.comprobarContacto(contacto);
+		
+		assertEquals(contacto.getMail(), "");
+		assertEquals(result, false);
 	}
+	
+	
 	
 	public void testNombreVacio() throws Exception
 	{
+		/*
+		 *
+		 * 	(1.2) Test AltaContacto (linea 201) retorna false
+		 * 	(2.1) Test AltaContacto (linea 206) retorna true
+		 * 
+		 */
+		
+		boolean result;
 
+		contacto.setNombre("");
+		contacto.setMail("mail");
+		result = alta.comprobarContacto(contacto);
+		
+		assertEquals(contacto.getNombre(), "");
+		assertEquals(contacto.getMail(), "mail");
+		assertEquals(result, false);
 	}
 	
 	public void testExistenRegistros() throws Exception
 	{
-
+		/*
+		 *
+		 * 	(1.2) Test AltaContacto (linea 201) retorna false
+		 * 	(2.2) Test AltaContacto (linea 206) retorna false
+		 *	(3.1) Test AltaContacto (linea 211) retorna true
+		 *
+		 * 
+		 */
+		
+		boolean result;
+		
+		contacto.setNombre("Eduardo");
+		contacto.setMail("eduardo@gmail.com");
+		result = alta.comprobarContacto(contacto);
+		
+		assertEquals(contacto.getNombre(), "Eduardo");
+		assertEquals(contacto.getMail(), "eduardo@gmail.com");
+		assertEquals(result, false);
 	}
 	
 	public void testNoExistenRegistros() throws Exception
 	{
-
+		/*
+		 *
+		 * 	(1.2) Test AltaContacto (linea 201) retorna false
+		 * 	(2.2) Test AltaContacto (linea 206) retorna false
+		 *	(3.2) Test AltaContacto (linea 211) retorna false
+		 * 
+		 */
+		
+		boolean result;
+		
+		contacto.setNombre("nombre");
+		contacto.setMail("mail");
+		result = alta.comprobarContacto(contacto);
+		
+		assertEquals(contacto.getNombre(), "nombre");
+		assertEquals(contacto.getMail(), "mail");
+		assertEquals(result, true);
 	}
 	
 	public void testExisteContacto() throws Exception
 	{
+		/*
+		 *
+		 * 	(1.2) Test AltaContacto (linea 201) retorna false
+		 * 	(2.2) Test AltaContacto (linea 206) retorna false
+		 *	(3.1) Test AltaContacto (linea 211) retorna true
+		 *	(4.1) Test AltaContacto (linea 213) retorna true
+		 * 
+		 */
+		
+		boolean result;
+		
+		contacto.setNombre("Eduardo");
+		contacto.setMail("eduardo@gmail.com");
+		result = alta.comprobarContacto(contacto);
+		
+		assertEquals(contacto.getNombre(), "Eduardo");
+		assertEquals(contacto.getMail(), "eduardo@gmail.com");
 
+		assertEquals(result, false);
 	}
 	
 	public void testNoExisteContacto() throws Exception
 	{
-
+		/*
+		 *
+		 * 	(1.2) Test AltaContacto (linea 201) retorna false
+		 * 	(2.2) Test AltaContacto (linea 206) retorna false
+		 *	(3.1) Test AltaContacto (linea 211) retorna true
+		 *	(4.2) Test AltaContacto (linea 213) retorna false
+		 * 
+		 */
+		
+		boolean result;
+		
+		contacto.setNombre("nombre");
+		contacto.setMail("mail");
+		result = alta.comprobarContacto(contacto);
+		
+		assertEquals(contacto.getNombre(), "nombre");
+		assertEquals(contacto.getMail(), "mail");
+		assertEquals(result, true);
 	}
 }
--- src/com/example/tests/LoginTest.java
+++ src/com/example/tests/LoginTest.java
@@ -22,6 +22,16 @@ public class LoginTest extends ActivityInstrumentationTestCase2<Login> {
 	 * 		cada if continene un and --> + 3 paths
 	 * 		total paths ==> 7 
 	 * 
+	 * 		Paths:
+	 * 			(1) Test Login (linea 72) retorna true
+	 *			(2) Test Login (linea 72) retorna false
+	 * 			(3) Test Login (linea 76) retorna true
+	 * 			(4) Test Login (linea 76) retorna false
+	 * 			(5) Test Login (linea 80) retorna true
+	 * 			(6) Test Login (linea 80) retorna false
+	 * 			(7) Test Login (linea 84) retorna true
+	 * 
+	 * 
 	 * 
 	 * A su vez también se estan realizando tests 
 	 * a las funciones getter/setter de la clase Usuario
@@ -59,26 +69,107 @@ public class LoginTest extends ActivityInstrumentationTestCase2<Login> {
 	
 	public void testUsuarioRellenado() throws Exception
 	{
+		/*
+		 *
+		 *  (1) Test Login (linea 72) retorna true
+		 * 
+		 */
+		
+		boolean result;
 
+		usuario.setNombre("admin");
+		usuario.setContrasenya("12345");
+		
+		result = login.validarUsuario(usuario);
+		
+		assertEquals(usuario.getNombre(), "admin");
+		assertEquals(usuario.getContrasenya(), "12345");
+		assertEquals(result, true);
 	}
-
+	
+	
+	
 	public void testNombreUsuarioVacio() throws Exception
 	{
+		/*
+		 *
+		 * 	(2) Test Login (linea 72) retorna false
+		 * 	(3) Test Login (linea 76) retorna true
+		 * 
+		 */
 		
+		boolean result;
+
+		usuario.setNombre("");
+		usuario.setContrasenya("1234");
+		result = login.validarUsuario(usuario);
+		assertEquals(usuario.getNombre(), "");
+		assertEquals(usuario.getContrasenya(), "1234");
+		assertEquals(result, false);
 	}
 	
 	public void testContrasenyaUsuarioVacia() throws Exception
 	{
-
+		/*
+		 *
+		 *	(2) Test Login (linea 72) retorna false
+		 *	(4) Test Login (linea 76) retorna false
+		 *	(5) Test Login (linea 80) retorna true
+		 * 
+		 */
+		
+		boolean result;
+		
+		usuario.setNombre("admin");
+		usuario.setContrasenya("");
+		result = login.validarUsuario(usuario);
+		assertEquals(usuario.getNombre(), "admin");
+		assertEquals(usuario.getContrasenya(), "");
+		assertEquals(result, false);
 	}
 	
 	public void testUsuarioVacio() throws Exception
 	{
+		/*
+		 *
+		 * 	(2) Test Login (linea 72) retorna false
+		 * 	(4) Test Login (linea 76) retorna false
+		 * 	(6) Test Login (linea 80) retorna false
+		 * 	(7) Test Login (linea 84) retorna true
+		 * 
+		 */
+		
+		boolean result;
 
+		usuario.setNombre("");
+		usuario.setContrasenya("");
+		result = login.validarUsuario(usuario);
+		
+		assertEquals(usuario.getNombre(), "");
+		assertEquals(usuario.getContrasenya(), "");
+		assertEquals(result, false);
 	}
 	
 	public void testLoginOk()
 	{
-
+		usuario.setNombre("admin");
+		usuario.setContrasenya("12345");
+		
+		//Access First value (edit-filed) and putting firstNumber value in it
+		EditText entradaUsuario = (EditText) solo.getView(R.id.TxtUsuario);
+		solo.clearEditText(entradaUsuario);
+		solo.enterText(entradaUsuario, String.valueOf(usuario.getNombre()));
+		
+		//Access Second value (edit-filed) and putting SecondNumber value in it
+		EditText entradaPassword = (EditText) solo.getView(R.id.TxtPassword);
+		solo.clearEditText(entradaPassword);
+		solo.enterText(entradaPassword, String.valueOf(usuario.getContrasenya()));
+		
+		Button botonLogin = (Button)solo.getCurrentActivity().findViewById(R.id.BtnEntrar);
+		
+		//Click on button
+		solo.clickOnButton(botonLogin.getText().toString()); 
+		solo.waitForActivity("com.example.informacion.MainActivity", 3000);
+		solo.assertCurrentActivity("The activity should be MainActivity ", "MainActivity");
 	}
 }
