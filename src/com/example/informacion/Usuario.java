0648245f3d7b0adb08324c5e65890150d29327c5
annablanc <blink1986@gmail.com>
2015-05-17 13:25:06 +0200

    Cambios en Usuario

--- src/com/example/informacion/Usuario.java
+++ src/com/example/informacion/Usuario.java
@@ -8,22 +8,19 @@ public class Usuario implements Parcelable {
 	private String nombre;
 	private String contrasenya;
 	private String mail;
-	private int algoritmoEncriptacion;
+	//private int algoritmoEncriptacion;
 	
 
 	@Override
 	public int describeContents() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public void writeToParcel(Parcel arg0, int arg1) {
-		// TODO Auto-generated method stub
 
 	}
 	public Usuario() {
-		// TODO Auto-generated constructor stub
 	}
 	
 	public Usuario(String nombre, String contrasenya, String mail) {
