c7644bb8f1b5aca3c03e19d94afdb5a6e73bea71
ejgutierrez74 <edufissure@msn.com>
2015-06-02 11:02:03 +0200

    Revisión de warnings

--- src/com/example/informacion/Login.java
+++ src/com/example/informacion/Login.java
@@ -12,9 +12,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 
